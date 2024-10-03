0074141632e8cb46b38028518c303b772c15c4b6
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-10-04 00:46:07 +0530

    changed console.log -> info

--- backend/db/mongoDb.js
+++ backend/db/mongoDb.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
+const { info } = require("../utility/logger.util");
 
 const mongoUrl = process.env.MONGO_URL;
 
 const mongoConnect = async () => {
   await mongoose.connect(mongoUrl);
-  console.log("Connected To Database Successfully");
+  info("Connected To Database Successfully");
 };
 
 module.exports = mongoConnect;
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const { app } = require("./app");
 const mongoConnect = require("./db/mongoDb.js");
+const { info } = require("./utility/logger.util.js");
 
 const PORT = process.env.PORT || 3001;
 
 const startServer = async () => {
   await mongoConnect();
   app.listen(PORT, () => {
-    console.log(`server started at port ${PORT}`);
+    info(`server started at port ${PORT}`);
   });
 };
 
